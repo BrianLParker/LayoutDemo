423ff1a0b409e5e114cde3961dbae4ac6a94bd9b
Brian Parker <brian.parker@hotmail.com.au>
2020-10-08 22:12:45 +1100

    Layout Setter

---
+++ LayoutDemo/Views/Components/LayoutSetter.cs
@@ -0,0 +1,22 @@
+﻿namespace LayoutDemo.Views.Components
+{
+    using LayoutDemo.Shared;
+    using Microsoft.AspNetCore.Components;
+
+    public class LayoutSetter : ComponentBase
+    {
+        [CascadingParameter]
+        public MainLayout Layout { get; set; }
+
+        [Parameter]
+        public RenderFragment Header { get; set; }
+
+        [Parameter]
+        public RenderFragment Footer { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Layout.SetHeaderAndFooter(Header, Footer);
+        }
+    }
+}
